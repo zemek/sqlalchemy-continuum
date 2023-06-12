ae5c9f3ce2a587874c534fbfe467d79160abb8fd
Stephen Leicht <stephen@findheadway.com>
2023-06-12 15:15:52 -0600

    Pull in headway changes

--- sqlalchemy_continuum/transaction.py
+++ sqlalchemy_continuum/transaction.py
@@ -112,7 +112,7 @@ def create_triggers(cls):
 
 
 class TransactionFactory(ModelFactory):
-    model_name = 'Transaction'
+    model_name = 'VersionTransaction'
 
     def __init__(self, remote_addr=True):
         self.remote_addr = remote_addr
@@ -121,16 +121,16 @@ class TransactionFactory(ModelFactory):
         """
         Create Transaction class.
         """
-        class Transaction(
+        class VersionTransaction(
             manager.declarative_base,
             TransactionBase
         ):
-            __tablename__ = 'transaction'
+            __tablename__ = 'version_transaction'
             __versioning_manager__ = manager
 
             id = sa.Column(
                 sa.types.BigInteger,
-                sa.schema.Sequence('transaction_id_seq'),
+                sa.schema.Sequence('version_transaction_id_seq'),
                 primary_key=True,
                 autoincrement=True
             )
@@ -170,7 +170,7 @@ class TransactionFactory(ModelFactory):
                     for field in fields
                     if hasattr(self, field)
                 )
-                return '<Transaction %s>' % ', '.join(
+                return '<VersionTransaction %s>' % ', '.join(
                     (
                         '%s=%r' % (field, value)
                         if not isinstance(value, int)
@@ -183,5 +183,5 @@ class TransactionFactory(ModelFactory):
                 )
 
         if manager.options['native_versioning']:
-            create_triggers(Transaction)
-        return Transaction
+            create_triggers(VersionTransaction)
+        return VersionTransaction
